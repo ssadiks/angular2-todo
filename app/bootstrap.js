8628c1c127e8e4af490a3dfe14bae33f4c3410f0
unknown <sadiks.pro@gmail.com>
2016-03-13 19:38:51 +0100

    First commit

---
+++ app/bootstrap.js
@@ -0,0 +1,6 @@
+"use strict";
+var browser_1 = require('angular2/platform/browser');
+var app_1 = require('./app');
+var store_1 = require('./services/store');
+browser_1.bootstrap(app_1.default, [store_1.TodoStore]);
+//# sourceMappingURL=bootstrap.js.map
